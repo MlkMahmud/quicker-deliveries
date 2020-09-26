35269f6d661f922460c99b61f75c99474c5e7558
Malik Mahmud <almalikmahmud@gmail.com>
2020-09-26 19:56:49 +0100

    refactor: rename controller files

---
+++ server/controllers/baseController.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { Location, Shop } from '../models';
+import { getLocations } from '../utils';
+
+async function saveShop(shopName, accessToken) {
+  const shop = await Shop.findById(shopName);
+  if (!shop) {
+    const locations = await getLocations(shopName, accessToken);
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await Shop.create([{ _id: shopName, balance: 0.3 }], { session });
+    await Location.insertMany(locations, { session });
+    await session.commitTransaction();
+  }
+}
+
+async function getShopData(shopName) {
+  const { _id, balance } = await Shop.findById(shopName);
+  const locations = await Location.find({ shop: shopName });
+  return { _id, balance, locations };
+}
+
+export default {
+  getShopData,
+  saveShop,
+};
