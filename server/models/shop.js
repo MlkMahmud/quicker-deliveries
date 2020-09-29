7d76cbd26f47f54dedd49a790c14e4543173de68
Malik Mahmud <almalikmahmud@gmail.com>
2020-09-29 20:30:42 +0100

    refactor(model:shop): add default value for balance field

--- server/models/shop.js
+++ server/models/shop.js
@@ -2,7 +2,10 @@ import { model, Schema } from 'mongoose';
 
 const schema = new Schema({
   _id: String,
-  balance: Number,
+  balance: {
+    type: Number,
+    default: 0.5,
+  },
 });
 
 export default model('Shop', schema);
