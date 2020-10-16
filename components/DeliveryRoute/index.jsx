b0ab9f44b47b6fb5e43b68cb6d967db2c569ada5
Malik Mahmud <almalikmahmud@gmail.com>
2020-10-16 12:53:03 +0100

    add social share links

--- components/DeliveryRoute/index.jsx
+++ components/DeliveryRoute/index.jsx
@@ -1,5 +1,10 @@
 import React, { useRef } from 'react';
-import { Button, Modal, TextField } from '@shopify/polaris';
+import {
+  Button, Modal, TextField,
+} from '@shopify/polaris';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faAt, faMapMarkerAlt, faSms } from '@fortawesome/free-solid-svg-icons';
 import { useStore } from '../../store';
 
 export default () => {
@@ -27,27 +32,94 @@ export default () => {
         },
       });
     }
-    console.log(result);
   };
 
   return (
     <Modal
       open={showDeliveryRoute}
-      sectioned
-      title="Share delivery route"
+      title="Delivery route"
       onClose={() => dispatch({ type: 'UNSET_ROUTE_URL' })}
     >
-      <div ref={inputRef}>
-        <TextField
-          value={deliveryRouteUrl}
-          readOnly
-          label="directions url"
-          labelHidden
-          connectedRight={
-            <Button primary onClick={copy}>Copy Link</Button>
-          }
-        />
-      </div>
+      <Modal.Section>
+        <div ref={inputRef}>
+          <TextField
+            helpText="Google maps link"
+            value={deliveryRouteUrl}
+            readOnly
+            label="directions url"
+            labelHidden
+            connectedRight={
+              <Button primary onClick={copy}>Copy</Button>
+            }
+          />
+        </div>
+      </Modal.Section>
+      <Modal.Section>
+        <div
+          style={{
+            marginBottom: '20px',
+            textDecoration: 'underline',
+            textAlign: 'center',
+          }}
+        >
+          <b>Share delivery route</b>
+        </div>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(4, 1fr)',
+          }}
+        >
+          <a
+            href={`https://wa.me?text=${encodeURI(deliveryRouteUrl)}`}
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              backgroundColor: '#25d366',
+              padding: '20px',
+              textAlign: 'center',
+            }}
+          >
+            <FontAwesomeIcon color="white" icon={faWhatsapp} size="2x" style={{ verticalAlign: 'middle' }} />
+          </a>
+          <a
+            href={`sms:;?&body=${encodeURI(deliveryRouteUrl)}`}
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              backgroundColor: '#000',
+              padding: '20px',
+              textAlign: 'center',
+            }}
+          >
+            <FontAwesomeIcon color="white" icon={faSms} size="2x" style={{ verticalAlign: 'middle' }} />
+          </a>
+          <a
+            href={`mailto:?body=${encodeURI(deliveryRouteUrl)}`}
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              backgroundColor: '#DB4437',
+              padding: '20px',
+              textAlign: 'center',
+            }}
+          >
+            <FontAwesomeIcon color="white" icon={faAt} size="2x" style={{ verticalAlign: 'middle' }} />
+          </a>
+          <a
+            href={deliveryRouteUrl}
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              backgroundColor: '#4285F4',
+              padding: '20px',
+              textAlign: 'center',
+            }}
+          >
+            <FontAwesomeIcon color="white" icon={faMapMarkerAlt} size="2x" style={{ verticalAlign: 'middle' }} />
+          </a>
+        </div>
+      </Modal.Section>
     </Modal>
   );
 };
